bd1af70d54e9e85edc7a20b9e8139f253d2f687e
23120320 <23120320@student.hcmus.edu.vn>
2025-12-14 09:49:52 +0700

    feat(layout): Add Navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
 function NavBar() {
-  return;
+  return (
+    <nav className="bg-[#e6e9f2] shadow-md border-b">
+      <div className="max-w-[1200px] mx-auto px-4 flex items-center h-14">
+        <Link to="/" className="mr-6">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="hover:bg-transparent cursor-pointer"
+          >
+            <span className="text-2xl">🏠︎</span>
+          </Button>
+        </Link>
+
+        <div className="flex flex-1 justify-end items-center space-x-2">
+          <div className="relative">
+            <Input
+              type="text"
+              placeholder="Search"
+              className="w-[200px] bg-white focus-visible:ring-0"
+            />
+          </div>
+          <Button className="border rounded cursor-pointer">Search</Button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
